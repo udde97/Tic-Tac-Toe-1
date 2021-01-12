cc03938b66218e547faece640f76bacc43c567b8
mahamudabukar <maab970514@student.jenseneducation.se>
2021-01-12 15:10:55 +0100

    Initial Commit

---
+++ Tic Tac Toe 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe 1
+//
+//  Created by Mahamud abukar on 2021-01-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
